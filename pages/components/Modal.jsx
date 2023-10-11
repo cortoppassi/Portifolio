c7337ecf17b102de368a11ced1a2086521b92c5e
Jonathan <jonathan-516@hotmail.com>
2023-10-11 18:28:45 -0300

    First Commit

---
+++ pages/components/Modal.jsx
@@ -0,0 +1,160 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 500,
+  p: 4,
+  backgroundColor: '#333',
+};
+
+const modalStyle = {
+  position: 'fixed',
+  bottom: '20px',
+  right: '20px',
+};
+
+const chatBotStyle = {
+  backgroundColor: '#333',
+  color: 'white',
+  padding: '16px',
+  borderRadius: '8px',
+};
+
+export default function ChatbotModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div style={modalStyle}>
+      <img
+        src="https://github.com/cortoppassi/Assistente-OpenAI/blob/main/image/ia.gif?raw=true"
+        alt=""
+        style={{
+          width: '130px',
+          height: '130px',
+          zIndex: '9999',
+          clipPath: 'circle(30px at center)',
+          objectFit: 'cover',
+        }}
+        onClick={handleOpen}
+      />
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <ChatGPTApp />
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+function ChatGPTApp() {
+  const [pergunta, setPergunta] = React.useState('');
+  const [resposta, setResposta] = React.useState('');
+  const [apiKey, setApiKey] = React.useState('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');
+  const [loading, setLoading] = React.useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    if (!apiKey) {
+      setResposta('Necessário colocar a chave da API no arquivo custom.js');
+      setLoading(false);
+      return;
+    }
+
+    setResposta('');
+    try {
+      const response = await fetch('https://api.openai.com/v1/completions', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + apiKey,
+        },
+        body: JSON.stringify({
+          model: 'text-davinci-003',
+          prompt: pergunta,
+          max_tokens: 20,
+          temperature: 0.5,
+        }),
+      });
+
+      const data = await response.json();
+      const answer = data.choices[0].text;
+      setResposta(answer);
+    } catch (error) {
+      setResposta('Sem resposta');
+    }
+
+    setLoading(false);
+  }
+
+  const chatBotStyle = {
+    backgroundColor: '#333',
+    color: 'white',
+    padding: '16px',
+    borderRadius: '8px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
+
+  const inputContainerStyle = {
+    display: 'flex',
+    flexDirection: 'row', // Alterado para linha (horizontal)
+    alignItems: 'center',
+  };
+
+  const inputStyle = {
+    backgroundColor: '#222',
+    color: 'white',
+    padding: '8px',
+    flex: 1, // Ocupa todo o espaço disponível
+    marginRight: '16px', // Espaço entre o campo de texto e o botão
+    overflow: 'hidden',
+  };
+
+  const buttonStyle = {
+    width: '100px', // Largura do botão
+  };
+
+
+
+  return (
+    <div style={chatBotStyle}>
+      
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <p style={{ color: 'white' }}>{pergunta}</p>
+        <p style={{ color: 'white' }}> {resposta}</p>
+      </div>
+      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <div style={inputContainerStyle}>
+          <textarea
+            rows="1"
+            cols="40"
+            placeholder="Digite a pergunta"
+            value={pergunta}
+            onChange={(e) => setPergunta(e.target.value)}
+            style={inputStyle}
+          ></textarea>
+          <Button variant="contained" type="submit" disabled={loading} style={buttonStyle}>
+            {loading ? 'Pesquisando...' : 'Enviar'}
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ pages/components/template/Album.jsx
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Modal from '../Modal'
+
+const defaultTheme = createTheme();
+
+export default function Album() {
+  const [repos, setRepos] = useState([]);
+  const selectedRepos = ['Assistente-OpenAI', 'Marionetista-Puppeteer', 'Rede-Neural'];
+
+  useEffect(() => {
+    const token = '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';
+    const apiUrl = 'https://api.github.com/user/repos';
+
+    fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Authorization': `token ${token}`,
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Filtra os repositórios específicos com base nos nomes
+      const filteredRepos = data.filter(repo => selectedRepos.includes(repo.name));
+      setRepos(filteredRepos);
+    })
+    .catch(error => {
+      console.error('Ocorreu um erro ao acessar a API do GitHub:', error);
+    });
+  }, []);
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <main style={{ backgroundColor: 'white' }}>
+        <Box
+          sx={{
+            bgcolor: '#f5f5f5',
+            minHeight: '100vh',
+            pt: 8,
+            pb: 6,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Container maxWidth="md">
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={4}>
+                <img
+                  src="https://media.licdn.com/dms/image/D4D03AQFCRG9dOfKk5g/profile-displayphoto-shrink_800_800/0/1695567050459?e=1702512000&v=beta&t=m1ayRzeMsYTLIfHa2TYybEJ5ulYqIQDbATa1OWjf3_o"
+                  alt=""
+                  style={{ borderRadius: '50%', width: '100%', border: 'solid 1px black' }}
+                />
+              </Grid>
+              <Grid item xs={12} md={8}>
+                <Typography
+                  component="h1"
+                  variant="h3"
+                  align="left"
+                  color="text.primary"
+                  gutterBottom
+                >
+                  Jonathan Cortoppassi
+                </Typography>
+                <Typography variant="h5" align="left" color="text.secondary" paragraph>
+                Cursando Análise e Desenvolvimento de Sistemas 
+                  Durante minha jornada acadêmica, adquiri experiência prática em desenvolvimento de projetos relacionados a inteligência artificial,
+                  chatbots e na criação de jogos em JavaScript, fazendo uso de tecnologias como React e Node.js.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+          
+        <Container sx={{ py: 8}} maxWidth="md">
+          <Grid container spacing={4}>
+            {repos.map(repo => (
+              <Grid item key={repo.name} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="div"
+                    sx={{
+                      pt: '56.25%',
+                    }}
+                    image="https://source.unsplash.com/random?wallpapers"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {repo.name}
+                    </Typography>
+                    <Typography>
+                      {repo.description || 'No description available'}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                      Repositório
+                    </Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Modal/>
+        
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Conecte-se comigo para colaborações e oportunidades futuras.
+        </Typography>
+
+        <Typography variant="subtitle1" align="center" color="#0e76a8" component="p">
+          <a href='https://www.linkedin.com/in/johncortoppassi/' target="_blank" rel="noopener noreferrer">
+          LinkedIn
+          </a>
+        </Typography>
+        
+      </Box>
+    </ThemeProvider>
+  );
+}

a7697caf363026590041e2682ecde2096a1aef7d
Jonathan Cortoppassi <jonathan-516@hotmail.com>
2025-02-08 22:42:35 -0300

    Feat: message about

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -42,17 +42,23 @@ const About = () => {
         />
       </div>
       <h2>Full Stack Developer!</h2>
-      <p style={{ textAlign: "center", fontSize: "1rem", lineHeight: "1.5", maxWidth: "600px" }}>
-        Como Analista Desenvolvedor na SEIDOR, sou responsável por criar
-        soluções de IA para atender a diversas necessidades empresariais, com
-        ênfase na entrega de qualidade e otimização de custos. Desenvolvimento e
-        implementação de chatbots e assistentes virtuais, proporcionando um
-        atendimento automatizado e eficiente. Gerenciamento de instâncias EC2 em
-        ambientes de nuvem, com foco em escalabilidade e segurança. Automação de
-        navegação através do protocolo DevTools para facilitar a interação com
-        sistemas externos. Experiência em integração de APIs, otimização de
-        desempenho web e implementação de novas funcionalidades para projetos de
-        grande porte.
+      <p
+        style={{
+          textAlign: "center",
+          fontSize: "1rem",
+          lineHeight: "1.5",
+          maxWidth: "600px",
+        }}
+      >
+        As a Developer Analyst at SEIDOR, I am responsible for creating AI
+        solutions to meet various business needs, with an emphasis on quality
+        delivery and cost optimization. Development and implementation of
+        chatbots and virtual assistants, providing automated and efficient
+        service. Management of EC2 instances in cloud environments, focusing on
+        scalability and security. Navigation automation through the DevTools
+        protocol to facilitate interaction with external systems. Experience in
+        integrating APIs, optimizing web performance and implementing new
+        features for large projects.
       </p>
       <button style={{ backgroundColor: "#00a5ec", color: "#081b29" }}>
         Read More
